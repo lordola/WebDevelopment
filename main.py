10d3ae56008942b63b89df446a169cc6ede79a87
LUKMAN ADEKUNLE <113257002+lordola@users.noreply.github.com>
2024-07-02 12:51:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, request, flash
-
-app = Flask(__name__)
-app.secret_key = "lukmans_aderwj234575"
-
-
-@app.route('/')
-def home():
-    #flash("What's your name?")
-    return render_template('index.html')
-
+# print("Welcome to the Band Name Generator")
 #
-# @app.route('/greet')
-# def greet():
-#     flash("Hi " + str(request.form['name_input']) + ", Glad to meet you")
-#     return render_template('index.html')
+# name = input("What is your name\n")
+# city = input("What is your city\n")
+# print(len(name))
+# print("Your brand name is " + name + " " + city)
+# print("Hello world\nHello World\nHello world")
+#
+#
+# # input will be executed first, followed by print statement
+# print("Hello " + input("Whats is your name"))
+print("Thank you for choosing Python Pizza Deliveries")
+size = input("What size did you want? S , M, or L\n")
+add_pepperoni= input("Did you want pepperoni? Y or N\n")
+extra_cheese= input("Did you want extra cheese? Y or N\n")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size =="S":
+        bill +=2
+    else:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is {bill}")
